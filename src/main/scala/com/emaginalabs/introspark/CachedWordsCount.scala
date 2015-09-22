974c878048d6edffc442f2c4258965d70557f8ba
Sergio Arroyo <sergio@emaginalabs.com>
2015-09-22 20:24:28 +0200

    Add hello world with cache

---
+++ src/main/scala/com/emaginalabs/introspark/CachedWordsCount.scala
@@ -0,0 +1,41 @@
+package com.emaginalabs.introspark
+
+import org.apache.commons.cli.{CommandLine, Options}
+import org.apache.spark.SparkContext
+
+/**
+ * @author Sergio Arroyo - @delr3ves
+ */
+
+object CachedWordsCount extends SparkLauncherUtils {
+
+  val FILE_OPTION = "file"
+
+  override def getAppName(): String = "Words counter cached"
+
+  override def addSpecificOptions(options: Options): Options = {
+    options.addOption(FILE_OPTION, true, "The input file where the words are")
+  }
+
+  override def execute(spark: SparkContext, options: CommandLine): Unit = {
+    val fileRDD = spark.textFile(options.getOptionValue(FILE_OPTION))
+    val wordsRDD = fileRDD.flatMap(row => {
+      println("Found a row")
+      row.split(" ")
+    })
+
+    println("Counting words again with no cache ....")
+    println("Found words:" + wordsRDD.count())
+    println("Counting words again with no cache so will show messages again ....")
+    Thread.sleep(5000)
+    println("Found words:" + wordsRDD.count)
+
+    wordsRDD.cache
+
+    println(s"NOW WITH CACHE will perform operation last time!!!!!")
+    println("Found words:" +wordsRDD.count())
+    println("Counting words again with cache ....")
+    println("Found words:" +wordsRDD.count())
+  }
+
+}
