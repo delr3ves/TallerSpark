2ea2f8306ba2d5c21ffecedfe25dc51ef2bf93a7
fdeatilano <fdeatilano@gmail.com>
2015-09-25 03:57:37 +0200

    Use spark sql with csv data with header

--- src/main/scala/com/emaginalabs/introspark/SparkSqlCsvWithHeader.scala
+++ src/main/scala/com/emaginalabs/introspark/SparkSqlCsvWithHeader.scala
@@ -9,11 +9,11 @@ import org.apache.spark.sql.types.{StringType, StructField, StructType}
  * @author Francisco de Atilano
  */
 
-object SparkSqlJsonWithoutSchema extends SparkLauncherUtils {
+object SparkSqlCsvWithHeader extends SparkLauncherUtils {
 
   val FILE_OPTION = "file"
-
-  override def getAppName(): String = "Managing structured data with Spark SQL: JSON without schema"
+  
+  override def getAppName(): String = "Managing structured data with Spark SQL: CSV with header"
 
   override def addSpecificOptions(options: Options): Options = {
     options.addOption(FILE_OPTION, true, "The input file where the cell towers data is")
@@ -22,12 +22,15 @@ object SparkSqlJsonWithoutSchema extends SparkLauncherUtils {
   override def execute(spark: SparkContext, options: CommandLine): Unit = {
     val sparkSql = new SQLContext(spark)
 
-    val towersDataFrame = sparkSql.read.json(options.getOptionValue(FILE_OPTION))
+    val towersDataFrame = sparkSql.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .load(options.getOptionValue(FILE_OPTION))
     towersDataFrame.printSchema()
 
     towersDataFrame.registerTempTable("towers")
-    val filteredTowers = sparkSql.sql("SELECT radio,cell FROM towers WHERE area = 801 AND range > 300")
-    filteredTowers.groupBy("radio").count().show()
+    val filteredTowers = sparkSql.sql("SELECT radio,cell FROM towers WHERE area = 801")
+    filteredTowers.show()
 
   }
 
