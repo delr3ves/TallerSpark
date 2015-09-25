644bd1165ac5d980f05e60d25f4ebe584e40feeb
fdeatilano <fdeatilano@gmail.com>
2015-09-25 02:05:32 +0200

    Manage data in csv format

--- src/main/scala/com/emaginalabs/introspark/StructuredDataCsv.scala
+++ src/main/scala/com/emaginalabs/introspark/StructuredDataCsv.scala
@@ -22,22 +22,13 @@ object StructuredDataCsv extends SparkLauncherUtils {
 
   override def execute(spark: SparkContext, options: CommandLine): Unit = {
     val linesRDD: RDD[String] = spark.textFile(options.getOptionValue(FILE_OPTION))
-    val towersRDD: RDD[CellTower] = linesRDD.filter(!_.contains(HEADER)).map(line => {
-      val values = line.split(",").toList
-      CellTower.tupled(
-        values match {
-          case List(radio: String, mcc: String, net: String, area: String,cell: String,
-                    unit: String, lon: String, lat: String, range: String, samples: String,
-                    changeable: String, created: String, updated: String, averageSignal: String)
-          =>
-               (radio, mcc, net, area, cell,
-                 unit, lon, lat, range, samples,
-                 changeable, created, updated, averageSignal)
-        }
-      )
-    })
+    val towersRDD: RDD[CellTower] = linesRDD
+      .filter(!_.contains(HEADER))
+      .map(_.split(",").toList)
+      .filter(_.size >= 14)
+      .map(values => new CellTower(values(0), values(1), values(2), values(3), values(4), values(5), values(6), values(7), values(8), values(9), values(10), values(11), values(12), values(13)))
 
-    towersRDD.foreach(tower => println(tower.productIterator.toList))
+    towersRDD.foreach(tower => println(tower))
   }
 
 }
--- src/main/scala/com/emaginalabs/introspark/beans/CellTower.scala
+++ src/main/scala/com/emaginalabs/introspark/beans/CellTower.scala
@@ -4,18 +4,19 @@ package com.emaginalabs.introspark.beans
  * Created by Francisco de Atilano
  */
 case class CellTower(val radio: String,
-                val mcc: String,
-                val net: String,
-                val area: String,
-                val cell: String,
-                val unit: String,
-                val lon: String,
-                val lat: String,
-                val range: String,
-                val samples: String,
-                val changeable: String,
-                val created: String,
-                val updated: String,
-                val averageSignal: String) {
+                     val mcc: String,
+                     val net: String,
+                     val area: String,
+                     val cell: String,
+                     val unit: String,
+                     val lon: String,
+                     val lat: String,
+                     val range: String,
+                     val samples: String,
+                     val changeable: String,
+                     val created: String,
+                     val updated: String,
+                     val averageSignal: String) {
 
+  override def toString = radio + "," + mcc + "," + net + "," + area + "," + cell + "," + unit + "," + lon + "," + lat + "," + range + "," + samples + "," + changeable + "," + created + "," + updated + "," + averageSignal
 }
